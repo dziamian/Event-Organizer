3a392c29d2dfc59fa88a478ae6568b9ead3da440
dziamian <56453197+dziamian@users.noreply.github.com>
2020-05-17 14:58:06 +0200

    Update dbRepoToLocal.bat
    Updating script to import database to localhost.

--- Database/dbRepoToLocal.bat
+++ Database/dbRepoToLocal.bat
@@ -2,4 +2,4 @@ set CURR_PATH=%cd%
 set MONGODB_PATH=C:\Program Files\MongoDB\Server\4.2\bin
 cd %MONGODB_PATH%
 %MONGODB_PATH:~0,2%
-mongorestore --drop -d guideDB %CURR_PATH%\data
+mongorestore --drop -d guideDB %CURR_PATH%\guideDB
